7a863254506fc5bbc6d7739d87b1f439e9827833
thifnmi <thifnmi@thifnmis-MacBook-Pro.local>
2023-07-04 23:58:48 +0700

    change variable name & modify model

--- pkg/domain/example.go
+++ pkg/domain/example.go
@@ -6,31 +6,33 @@ import (
 )
 
 type Example struct {
-	ID          int        `gorm:"primaryKey"`
-	Uuid        uuid.UUID  `json:"uuid" gorm:"not null"`
-	CreatedAt   *time.Time `json:"created_at"`
-	UpdatedAt   *time.Time `json:"updated_at"`
-	DeletedAt   *time.Time `json:"deleted_at"`
+	ID        int        `gorm:"primaryKey"`
+	Uuid      uuid.UUID  `json:"uuid" gorm:"not null"`
+	Name      string     `json:"name"`
+	CreatedAt *time.Time `json:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at"`
 }
 
 type BaseExampleResponse struct {
-	Uuid        uuid.UUID  `json:"uuid"`
-	CreatedAt   *time.Time `json:"created_at"`
+	Uuid      uuid.UUID  `json:"uuid"`
+	Name      string     `json:"name"`
+	CreatedAt *time.Time `json:"created_at"`
 }
 
 type ExampleResponse struct {
-	Success   bool            `json:"success"`
-	ErrorCode int16           `json:"error_code"`
-	Message   string          `json:"message"`
+	Success   bool                `json:"success"`
+	ErrorCode int16               `json:"error_code"`
+	Message   string              `json:"message"`
 	Data      BaseExampleResponse `json:"data"`
 }
 
 type ListExampleResponse struct {
-	Success   bool              `json:"success"`
-	ErrorCode int16             `json:"error_code"`
-	Message   string            `json:"message"`
+	Success   bool                  `json:"success"`
+	ErrorCode int16                 `json:"error_code"`
+	Message   string                `json:"message"`
 	Data      []BaseExampleResponse `json:"data"`
-	Metadata  MetadataResponse  `json:"metadata"`
+	Metadata  MetadataResponse      `json:"metadata"`
 }
 
-type ExampleQuery struct {}
+type ExampleQuery struct{}
